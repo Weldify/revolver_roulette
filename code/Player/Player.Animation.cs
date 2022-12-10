85acb958699596413f7e95cc4f97766abc6d8bb1
Ian <skalenowsky@gmail.com>
2022-12-10 21:56:33 +0300

    fix anims

--- code/Player/Player.Animation.cs
+++ code/Player/Player.Animation.cs
@@ -4,16 +4,11 @@ public partial class Player
 {
 	void DoPlayerAnimation()
 	{
+		if ( Controller is null ) return;
+
 		// where should we be rotated to
 		var turnSpeed = 0.02f;
-
-		Rotation rotation;
-
-		// If we're a bot, spin us around 180 degrees.
-		if ( Client.IsBot )
-			rotation = ViewAngles.WithYaw( ViewAngles.yaw + 180f ).ToRotation();
-		else
-			rotation = ViewAngles.ToRotation();
+		var rotation = ViewAngles.ToRotation();
 
 		var idealRotation = Rotation.LookAt( rotation.Forward.WithZ( 0 ), Vector3.Up );
 		Rotation = Rotation.Slerp( Rotation, idealRotation, Controller.WishVelocity.Length * Time.Delta * turnSpeed );
@@ -23,7 +18,7 @@ public partial class Player
 
 		animHelper.WithWishVelocity( Controller.WishVelocity );
 		animHelper.WithVelocity( Controller.Velocity );
-		animHelper.WithLookAt( EyePosition + EyeRotation.Forward * 100.0f, 1.0f, 1.0f, 0.5f );
+		animHelper.WithLookAt( AimRay.Position + rotation.Forward * 100.0f, 1.0f, 1.0f, 0.5f );
 		animHelper.AimAngle = rotation;
 		animHelper.FootShuffle = shuffle;
 		animHelper.DuckLevel = MathX.Lerp( animHelper.DuckLevel, Controller.HasTag( "ducked" ) ? 1 : 0, Time.Delta * 10.0f );
